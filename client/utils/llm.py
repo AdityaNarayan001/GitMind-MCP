3e55a32c546e63419c2e2162e4e9f256e17da133
Aditya Narayan <aditya.narayan@juspay.in>
2025-03-24 12:20:37 +0530

    added temp param in llm()

--- client/utils/llm.py
+++ client/utils/llm.py
@@ -5,14 +5,24 @@ from utils.system_prompt import sys_prompt
 
 def llm(available_features, content):
     messages = [
-        {"role": "system", "content": 'Available Features : ' + f'{available_features}' + 'Instructions : ' + f'{sys_prompt}'},
+        {"role": "system", "content": 'Instructions : ' + f'{sys_prompt}' + 'Available Features : ' + f'{available_features}'},
         {"role": "user", "content": f'{content}'}
     ]
 
-    response = ollama.chat(model="llama3.1", messages=messages)
+    response = ollama.chat(
+        model="llama3.1", 
+        messages=messages,
+        options= {
+            "temperature": 0.2
+            }
+        )
 
     try:
         json_response = json.loads(response['message']['content'])
         return json.dumps(json_response, indent=2)
     except json.JSONDecodeError:
         return "Error: Response is not valid JSON"
+    
+# TODO: Add chat context to the model. chat_history
+# TODO: improve response type to client.
+# TODO: build general MCP client
